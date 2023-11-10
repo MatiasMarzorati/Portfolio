60c64a0cf04607fdb11933fe46b0a997057910d2
MatiasMarzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2023-11-10 13:52:59 -0300

    subida operadores aritmeticos

---
+++ Udemy/JavaScript/operadores aritmeticos.js
@@ -0,0 +1,37 @@
+//operadores aritmeticos
+
+let a,b,c,d,e,f;
+
+//operdor suma
+
+a = 3+4;
+console.log(a)
+
+//operador resta
+
+b = 6-2;
+console.log(b);
+
+//multiplicacion
+
+c = a* 2;
+console.log(c);
+
+//division
+
+d = b / 2.5;
+console.log(d);
+console.log(typeof d);
+
+//modulo restante
+f = 9 % 2;
+console.log(f)
+
+//operador potencia solo en ES2016 -ECMAScript 2016
+e = a ** 3
+console.log(e)
+
+//sino
+
+e = Math.pow(2,3)
+console.log(e)
