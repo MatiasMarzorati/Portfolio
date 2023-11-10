9a043d0a67421e18aadfbb0ad34413e2a533436d
MatiasMarzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2023-11-10 01:19:52 -0300

    subida tipos de variables

---
+++ Udemy/JavaScript/Tipos de variables.js
@@ -0,0 +1,21 @@
+//tipos de datos en js
+
+let miEntero = 10;
+console.log(miEntero);
+
+miEntero = "Hola";
+console.log(miEntero);
+
+
+let miCadena = "Hola";
+console.log(miCadena);
+
+let miBoolean = true;
+console.log(miBoolean);
+
+let miNull = null;
+console.log(miNull);
+
+let miUndefined = undefined;
+console.log(miUndefined)
+
