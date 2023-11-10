6ee8187a9ebecbc26b4deb87c15413a952d7a525
MatiasMarzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2023-11-10 01:26:56 -0300

    tipos de variable

--- Udemy/JavaScript/Tipos de variables.js
+++ Udemy/JavaScript/Tipos de variables.js
@@ -2,6 +2,7 @@
 
 let miEntero = 10;
 console.log(miEntero);
+console.log(typeof miEntero)
 
 miEntero = "Hola";
 console.log(miEntero);
@@ -9,13 +10,16 @@ console.log(miEntero);
 
 let miCadena = "Hola";
 console.log(miCadena);
+console.log(typeof miCadena);
 
 let miBoolean = true;
 console.log(miBoolean);
+console.log(typeof miBoolean);
 
 let miNull = null;
 console.log(miNull);
+console.log(typeof miNull);
 
 let miUndefined = undefined;
 console.log(miUndefined)
-
+console.log(typeof miUndefined);
