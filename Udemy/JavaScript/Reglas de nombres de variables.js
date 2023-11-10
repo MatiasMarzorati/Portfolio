814fcc8ee81e678fa90c277291e58d568073bf07
MatiasMarzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2023-11-09 21:50:08 -0300

    subida de clase 3

---
+++ Udemy/JavaScript/Reglas de nombres de variables.js
@@ -0,0 +1,23 @@
+//reglas de nombres de variables
+
+// letras, $,_, posteriormente numeros
+
+let $miNumero1 = 10;
+let _Minumero2= 20;
+
+console.log($miNumero1);
+console.log(_Minumero2);
+
+
+let miNUmero3 = 30;
+
+console.log(miNUmero3);
+
+//es sensible a mayusculas y a minusculas.
+///si le pifiamos no lo va a entender
+
+/*
+comentario
+de varias lineas
+
+*/
