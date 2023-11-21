b0acabdd99a7e5d69433f1031b306e8733a2dafb
MatiasMarzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2023-11-21 13:19:49 -0300

    subida ejemplos de arreglos en js

---
+++ Udemy/JavaScript/EjemploArreglos.js
@@ -0,0 +1,12 @@
+// Arreglos en JS
+// Declaracion del arreglo
+let numerosArreglo = [];
+
+// Modificar los valores
+numerosArreglo[0] = 13;
+numerosArreglo[1] = 21;
+numerosArreglo[4] = 'hola';
+// Leer los valores
+console.log(`Elemento 1 [0]: ${numerosArreglo[0]}`);
+console.log(`Elemento 3 [2]: ${numerosArreglo[2]}`);
+console.log(`Elemento 5 [4]: ${numerosArreglo[4]}`);
