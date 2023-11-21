5bee2100f6076fe1d5237f59d6cd30c839e76c80
MatiasMarzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2023-11-20 21:09:15 -0300

    subida de operadores de asignacion

---
+++ Udemy/JavaScript/OperadoresAsignacion.js
@@ -0,0 +1,31 @@
+// Operadores de Asignacion
+let miNumero = 10;
+console.log(miNumero);
+
+miNumero = 20;
+console.log(miNumero);
+
+// Operador +=
+miNumero += 5;
+console.log(miNumero);
+
+// Operador -=
+miNumero -= 3;
+console.log(miNumero);
+
+// Operador *=
+miNumero *= 2;
+console.log(miNumero);
+
+// Operador /=
+miNumero /= 4;
+console.log(miNumero);
+
+// Operador %=
+miNumero %= 2;
+console.log(miNumero);
+
+// Exponente **=
+miNumero = 8;
+miNumero **= 2;// 8 * 8 = 64
+console.log(miNumero);
