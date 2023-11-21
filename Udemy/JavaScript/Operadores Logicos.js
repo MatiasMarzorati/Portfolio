dc456cc12047a58e75d43c0600147a0abd05da8e
MatiasMarzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2023-11-20 23:35:03 -0300

    Subida operadores logicos

---
+++ Udemy/JavaScript/Operadores Logicos.js
@@ -0,0 +1,19 @@
+//operadores logicos
+
+let a = true;
+let b = false;
+
+console.log(a);
+console.log(b);
+
+// operador logico AND
+
+console.log(`A AND B --> ${a && b}`);
+
+// operador logico OR
+
+console.log(`A AND B --> ${a || b}`);
+
+//Operador Not 
+
+console.log(!a)
