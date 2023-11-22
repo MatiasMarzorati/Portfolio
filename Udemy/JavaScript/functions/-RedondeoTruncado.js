8a9684aec97c4587bbbc20fa2952ad0dfba2f881
MatiasMarzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2023-11-22 20:00:16 -0300

    subida ejers

---
+++ Udemy/JavaScript/functions/-RedondeoTruncado.js
@@ -0,0 +1,14 @@
+// Redondeo y truncado en Java
+
+let numero = 8.5;
+
+// Redondeo
+// Math.round() redondea al valor entero mas cercano
+// .5 o superaior, redondea al valor entero superior mas cercano
+redondeo = Math.round(numero)
+console.log(redondeo)
+
+// Truncado
+// Math.trunc() elimina la parte flotante (decimal)
+truncado = Math.trunc(numero)
+console.log(truncado)
