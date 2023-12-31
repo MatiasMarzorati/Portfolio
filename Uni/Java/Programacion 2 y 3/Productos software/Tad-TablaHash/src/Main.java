8c6beb58153f40cbbb41011541a7d87a44b5f534
MatiasMarzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2023-12-31 16:06:37 -0300

    subida

--- Uni/Java/Programacion 2 y 3/Productos software/Tad-TablaHash/src/Main.java
+++ Uni/Java/Programacion 2 y 3/Productos software/Tad-TablaHash/src/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // INICIALIZACION DE SCANNER
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner  = new Scanner(System.in);
         Tabla t = null;
         // OPCION MENU PRIMER CONJUNTO
         int opcion;
