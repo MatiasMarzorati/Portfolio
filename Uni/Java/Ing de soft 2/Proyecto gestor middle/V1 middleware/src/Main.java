bed1e30a9035f6808aa6c48775d61fcd39c759f6
MatiasMarzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2024-03-12 19:36:10 -0300

    iniciacion de la v1

--- Uni/Java/Ing de soft 2/Proyecto gestor middle/V1 middleware/src/Main.java
+++ Uni/Java/Ing de soft 2/Proyecto gestor middle/V1 middleware/src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Por favor ingresar que desea hacer" +
+                "1 - Agregar cliente" +
+                "2 - Eliminar Cliente" +
+                "3 - Recordar que sucede hoy:" +
+                "4 - Buscar polizas por cliente" +
+                "5 - Buscar por numero de polizas" +
+                "6 - ");
 
 
 
-        
 
     }
 }
