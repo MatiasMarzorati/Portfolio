89e1b575d3383d7ac582a7b8ef58788ff3f0a263
Matias Marzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2024-06-21 00:47:26 -0300

    cambiado

--- Personal/Ing de soft/pythonProject/main.py
+++ Personal/Ing de soft/pythonProject/main.py
@@ -1,31 +1,46 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import os
+import json
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    tasks = []
+
+    def _set_headers(self, content_type="text/html"):
+        self.send_response(200)
+        self.send_header("Content-type", content_type)
+        self.end_headers()
+
     def do_GET(self):
-        # Define the path to the HTML file
-        html_file_path = "index.html"
-
-        # Check if the file exists
-        if os.path.exists(html_file_path):
-            # Read the content of the HTML file
-            with open(html_file_path, 'rb') as file:
-                html_content = file.read()
-
-            # Send response status code
-            self.send_response(200)
-            # Send headers
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            # Write the HTML content to the response
-            self.wfile.write(html_content)
+        if self.path == '/':
+            self.serve_html()
         else:
-            # If the file is not found, send a 404 response
             self.send_response(404)
-            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+    def serve_html(self):
+        html_file_path = "index.html"
+        try:
+            with open(html_file_path, 'rb') as file:
+                self._set_headers()
+                self.wfile.write(file.read())
+        except FileNotFoundError:
+            self.send_response(404)
             self.end_headers()
             self.wfile.write(b"404 Not Found")
 
+    def do_POST(self):
+        if self.path == '/add_task':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            task_data = json.loads(post_data)
+
+            task = {
+                'task': task_data['task'],
+                'description': task_data['description']
+            }
+            self.tasks.append(task)
+
+            self._set_headers("application/json")
+            self.wfile.write(json.dumps(task).encode('utf-8'))
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8080):
     server_address = ('', port)
