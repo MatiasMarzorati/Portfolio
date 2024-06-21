bc27cd308abe15071fd0252637a4ebd20bc79782
Matias Marzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2024-06-20 23:59:29 -0300

    actualizacion

--- Personal/Ing de soft/pythonProject/main.py
+++ Personal/Ing de soft/pythonProject/main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Define the path to the HTML file
+        html_file_path = "index.html"
+
+        # Check if the file exists
+        if os.path.exists(html_file_path):
+            # Read the content of the HTML file
+            with open(html_file_path, 'rb') as file:
+                html_content = file.read()
+
+            # Send response status code
+            self.send_response(200)
+            # Send headers
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            # Write the HTML content to the response
+            self.wfile.write(html_content)
+        else:
+            # If the file is not found, send a 404 response
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd on port {port}...')
+    httpd.serve_forever()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    run()
